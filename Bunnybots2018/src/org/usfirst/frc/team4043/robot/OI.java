1c8810fbe73ccf2ab1e61357823fe1282bbfede5
Zoe Langbecker <FRCZZLangbecc@gmail.com>
2018-10-25 20:35:21 -0700

    yee
    and i cannot stress this enough, haw

--- Bunnybots2018/src/org/usfirst/frc/team4043/robot/OI.java
+++ Bunnybots2018/src/org/usfirst/frc/team4043/robot/OI.java
@@ -12,6 +12,7 @@ package org.usfirst.frc.team4043.robot;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	//yee, and I cannot stress this enough, haw
 	//// CREATING BUTTONS
 	// One type of button is a joystick button which is any button on a
 	//// joystick.
