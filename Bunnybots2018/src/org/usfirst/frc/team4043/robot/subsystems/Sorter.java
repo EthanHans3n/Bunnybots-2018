c009093ff1729ae45e42266be41309961f244ea1
Zoe Langbecker <FRCZZLangbecc@gmail.com>
2018-11-29 18:40:59 -0800

    deleted both actual yeet commands

--- Bunnybots2018/src/org/usfirst/frc/team4043/robot/subsystems/Sorter.java
+++ Bunnybots2018/src/org/usfirst/frc/team4043/robot/subsystems/Sorter.java
@@ -12,13 +12,6 @@ public class Sorter extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 
-	public void actSualYeet() {
-		RobotMap.yeetR.set(1);
-	}
-
-	public void actualUnYeet() {
-		RobotMap.yeetL.set(-1);
-	}
 	
 	public void noYeet() {
 		RobotMap.yeetR.set(0);
